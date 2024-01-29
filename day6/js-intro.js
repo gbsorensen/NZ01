f9acb64153d70c62d3d11a51f00cd78d9059f8f5
Isabel Louise Vicente <isabellouiseph@gmail.com>
2024-01-29 16:57:37 +0800

    added day6

---
+++ day6/js-intro.js
@@ -0,0 +1,28 @@
+console.log("abbi");
+console.log(18);
+console.log("blue");
+
+function sayHello() {
+	console.log("Hello, world!");
+}
+sayHello();
+
+// Comments
+// This is a single line comment
+/* This
+is
+a
+multi
+line
+comment */
+
+// Variables
+let age = 18;
+age = 22;
+
+const name = "Abbi";
+
+// var
+
+console.log(age);
+console.log(name);
